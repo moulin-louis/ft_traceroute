8c709f06556cbc8663a2ead443ecf549401cf140
moulin-louis <louis.moulin@outlook.fr>
2024-02-22 14:25:25 +0100

    test tracerroute

--- inc/ft_traceroute.h
+++ inc/ft_traceroute.h
@@ -5,4 +5,16 @@
 #ifndef FT_TRACEROUTE_H
 #define FT_TRACEROUTE_H
 
+#include "libft.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+//PORT LIKELY UNUSED
+#define TC_PORT 33434
+
 #endif //FT_TRACEROUTE_H
--- src/main.c
+++ src/main.c
@@ -1,6 +1,43 @@
-#include <stdio.h>
+#include "ft_traceroute.h"
 
-int main() {
-  printf("Hello, World!\n");
+int main(int ac, char** av) {
+  struct sockaddr_in dest;
+  struct sockaddr_in rcv;
+  socklen_t len_recv;
+  if (ac == 1) {
+    fprintf(stderr, "Wrong nbr of args\n");
+    return 1;
+  }
+  int sckfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sckfd == -1)
+    return 1;
+  int ttl = 1;
+  if (setsockopt(sckfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) == -1) {
+    fprintf(stderr, "error setsockopt\n");
+    perror("setsockopt");
+    return 1;
+  }
+  ft_memset(&dest, 0, sizeof(dest));
+  dest.sin_family = AF_INET;
+  dest.sin_port = htons(TC_PORT);
+  dest.sin_addr.s_addr = inet_addr(av[1]);
+  int retval = sendto(sckfd, "toto", 4, 0, (struct sockaddr*)&dest, sizeof(dest));
+  if (retval == -1) {
+    fprintf(stderr, "error sendto\n");
+    perror("sendto");
+    return 1;
+  }
+  char buf[2048];
+  ft_memset(buf, 0, sizeof(buf));
+  printf("%d bytes send\n", retval);
+
+  retval = recvfrom(sckfd, buf, 2048, 0, (struct sockaddr*)&rcv, &len_recv);
+  if (retval == -1) {
+    fprintf(stderr, "error recvfrom\n");
+    perror("recvfrom");
+    return 1;
+  }
+  printf("%d bytes read\n", retval);
+  close(sckfd);
   return 0;
 }
