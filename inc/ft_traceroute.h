cf83d24af9701e9e4debc6e1ac8edb9855c0c24f
loumouli <louis.moulin@outlook.fr>
2024-02-23 16:06:47 +0100

    WIP, start adding loop logic

--- inc/ft_traceroute.h
+++ inc/ft_traceroute.h
@@ -7,14 +7,36 @@
 
 #include "libft.h"
 
-#include <stdio.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
-//PORT LIKELY UNUSED
+// PORT LIKELY UNUSED
 #define TC_PORT 33434
 
-#endif //FT_TRACEROUTE_H
+// Which protocol used to send for probes
+typedef enum {
+  E_ICMP = 1 << 0, // Use ICMP request
+  E_UDP = 1 << 1, // Use UDP packet
+} E_PROT;
+
+typedef struct {
+  uint64_t ttl; // current ttl (from 1 to ttl max)
+  uint64_t first_ttl; // first ttl to use(default to 1)
+  uint64_t ttl_max; // max ttl (default to 30)
+  uint16_t port; // port used to send the udp packet (default to TC_PORT)
+  uint64_t waittime; // time in seconds to wait for a ICMP response
+  E_PROT prot; // protocul used (default to E_UDP)
+  int sck; // socket to send the probe packet
+  int icmp_sck; // socket used to receive ICMP response
+  struct sockaddr_in dest;
+} t_tc;
+
+#endif // FT_TRACEROUTE_H
--- src/main.c
+++ src/main.c
@@ -1,58 +1,134 @@
 #include "ft_traceroute.h"
 
-int main(int ac, char** av) {
-  struct sockaddr_in dest;
-  struct sockaddr_in rcv;
-  socklen_t len_recv;
-  if (ac == 1) {
-    fprintf(stderr, "Wrong nbr of args\n");
+char timeout;
+t_tc trace;
+
+void cleanup(void) {
+  close(trace.sck);
+  close(trace.icmp_sck);
+}
+
+void handle_alarm(int sig) {
+  if (sig == SIGALRM)
+    timeout = true;
+}
+
+void handle_quit(int sig) {
+  if (sig == SIGTERM || sig == SIGINT) {
+    cleanup();
+    exit(sig);
+  }
+}
+
+int64_t change_ttl(int sock, uint64_t new_ttl) {
+  const int retval = setsockopt(sock, IPPROTO_IP, IP_TTL, &new_ttl, sizeof(new_ttl));
+  if (retval == -1) {
+    perror("setsockopt ttl");
     return 1;
   }
-  int sckfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sckfd == -1) {
+  return 0;
+}
+
+int32_t apply_arg(int ac, char** av) {
+  (void)trace;
+  (void)ac;
+  (void)av;
+  return 0;
+}
+
+int32_t init_tc(int ac, char** av) {
+  trace.sck = socket(AF_INET, SOCK_DGRAM, 0);
+  if (trace.sck == -1) {
     perror("socket1");
     return 1;
   }
-  int icmp_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-  if (icmp_sock == -1) {
+  trace.icmp_sck = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+  if (trace.icmp_sck == -1) {
     perror("socket2");
     return 1;
   }
-  printf("all sock open\n");
-  int ttl = 8;
-  if (setsockopt(sckfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) == -1) {
-    fprintf(stderr, "error setsockopt\n");
-    perror("setsockopt");
+  trace.first_ttl = 1;
+  trace.port = TC_PORT;
+  trace.prot = E_UDP;
+  trace.ttl = 1;
+  trace.ttl_max = 30;
+  trace.waittime = 5;
+  apply_arg(ac, av);
+  trace.dest.sin_family = AF_INET;
+  trace.dest.sin_port = htons(trace.port);
+  trace.dest.sin_addr.s_addr = inet_addr(av[1]);
+  if (setsockopt(trace.sck, IPPROTO_IP, IP_TTL, &trace.first_ttl, sizeof(trace.first_ttl)) == -1) {
+    perror("setsockopt_ttl");
     return 1;
   }
-  ft_memset(&dest, 0, sizeof(dest));
-  dest.sin_family = AF_INET;
-  dest.sin_port = htons(TC_PORT);
-  dest.sin_addr.s_addr = inet_addr(av[1]);
-  int retval = sendto(sckfd, "toto", 4, 0, (struct sockaddr*)&dest, sizeof(dest));
-  if (retval == -1) {
-    fprintf(stderr, "error sendto\n");
-    perror("sendto");
-    return 1;
-  }
-  char buf[2048];
-  ft_memset(buf, 0, sizeof(buf));
-  printf("%d bytes send\n", retval);
+  return 0;
+}
 
-  retval = recvfrom(icmp_sock, buf, 2048, 0, (struct sockaddr*)&rcv, &len_recv);
+int64_t send_probe(void) {
+  const int64_t retval = sendto(trace.sck, "toto", 4, 0, (struct sockaddr*)&trace.dest, sizeof(trace.dest));
   if (retval == -1) {
-    fprintf(stderr, "error recvfrom\n");
-    perror("recvfrom");
+    perror("sendto probe");
     return 1;
   }
-  printf("version == %d\n", buf[0] >> 4);
-  printf("ihl == %d\n", buf[0] & 0x0F);
-  printf("ttl == %d\n", buf[8]);
-  printf("ICMP type == %d\n", buf[20]);
-  printf("ICMP code == %d\n", buf[21]);
-  ft_hexdump(buf, retval, 0);
-  printf("%d bytes read\n", retval);
-  close(sckfd);
-  close(icmp_sock);
+  return 0;
+}
+
+int64_t wait_response(struct icmphdr* icmphdr, struct iphdr* iphdr) {
+  char buf[2048] = {0};
+  int64_t retval = 0;
+  alarm(trace.waittime);
+  while (timeout == false) {
+    retval = recvfrom(trace.icmp_sck, buf, 2048, MSG_DONTWAIT, NULL, NULL);
+    if (retval == -1) {
+      if (errno == EAGAIN || errno == EWOULDBLOCK)
+        continue;
+      perror("recvfrom");
+      return 1;
+    }
+    break;
+  }
+  if (timeout == true)
+    timeout = false;
+  ft_memcpy(iphdr, buf, sizeof(iphdr));
+  ft_memcpy(icmphdr, buf + sizeof(icmphdr), sizeof(icmphdr));
+  return 0;
+}
+
+int main(int ac, char** av) {
+  if (ac == 1) {
+    // display_usage();
+    fprintf(stderr, "Wrong nbr of args\n");
+    return 1;
+  }
+  if (init_tc(ac, av))
+    return 1;
+  fflush(NULL);
+  signal(SIGALRM, handle_alarm);
+  signal(SIGTERM, handle_quit);
+  signal(SIGINT, handle_quit);
+  for (uint64_t iter = 0; iter < trace.ttl_max; ++iter) {
+    struct iphdr iphdr;
+    struct icmphdr icmphdr;
+
+    // send the probe
+    send_probe();
+    // wait for the response
+    wait_response(&icmphdr, &iphdr);
+    printf("%lu: ", trace.ttl);
+    if (ft_memcmp(&icmphdr, (uint64_t[]){0, 0, 0, 0, 0, 0, 0, 0}, sizeof(icmphdr)) != 0) {
+      ft_hexdump(&icmphdr, sizeof(icmphdr), 0);
+    }
+    else {
+      printf("* * *\n");
+    }
+    fflush(NULL);
+    // stop if ICMP port reset
+    trace.ttl += 1;
+    if (change_ttl(trace.sck, trace.ttl)) {
+      break;
+    }
+    // start over with ttl += 1 otherwose
+  }
+  cleanup();
   return 0;
 }
